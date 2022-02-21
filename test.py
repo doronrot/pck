c7a9181cb6bdfb6f94f0b87474ac6edb19921f99
doronrot <doronrot17@gmail.com>
2022-02-21 22:34:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ TENBIS = "https://www.10bis.co.il/NextApi/GetRestaurantMenu?culture=en&uiCulture
 response = requests.get(TENBIS)
 print(response.json())
 
-# response2 = requests.get(TENBIS + "drinks/")
-# print(response2.json())
+response2 = requests.get(TENBIS + "drinks/")
+print(response2.json())
 
-# id_5 = 0
-# id_6 = 0
-# order1 = {"drinks": [id_5, id_6],
-#           "desserts": [id_5, id_6],
-#           "pizzas": [2055830, 2055831]}
-# response3 = requests.post(BASE + "tenbis/order", order1)
-# print(response3.json())
+id_5 = 0
+id_6 = 0
+order1 = {"drinks": [id_5, id_6],
+          "desserts": [id_5, id_6],
+          "pizzas": [2055830, 2055831]}
+response3 = requests.post(BASE + "tenbis/order", order1)
+print(response3.json())
 
